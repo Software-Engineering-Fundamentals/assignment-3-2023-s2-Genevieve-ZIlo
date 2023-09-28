661c181cdb37bd271b6a67d97ebd2fe949b466f0
s3951117 <s3951117@student.rmit.edu.au>
2023-09-28 12:13:58 +1000

    quiz class

---
+++ Quiz.java
@@ -0,0 +1,55 @@
+import java.sql.Date;
+
+public class Quiz {
+    private String quizCode;
+    private Double quizScore;
+    private Date quizDate;
+    private Double duration;
+    
+
+    public Quiz(String quizCode, Double quizScore, Date quizDate, Double duration){
+        this.quizCode = quizCode;
+        this.quizScore = quizScore;
+        this.quizDate = quizDate;
+        this.duration = duration;
+    }
+
+    //setters
+    public void setQuizCode(String quizCode){
+        this.quizCode = quizCode;
+    }
+
+    public void setQuizScore(Double quizScore){
+        this.quizScore = quizScore;
+    }
+
+    public void setQuizDate(Date quizDate){
+        this.quizDate = quizDate;
+    }
+
+    public void setDuration(Double duration){
+        this.duration = duration;
+    }
+
+    //getQuiz method
+
+    public String getQuizCode(){
+        return quizCode;
+    }
+
+    public Double getQuizScore(){
+        return quizScore;
+    }
+
+    public Date getQuizDate(){
+        return quizDate;
+    }
+
+    public Double getDuration(){
+        return duration;
+    }
+    
+    public boolean isPassed(double passThreshold) {
+        return quizScore >= passThreshold;
+    }
+}
